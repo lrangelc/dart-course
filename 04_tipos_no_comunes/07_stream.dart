8b4e44b76d3fd7147f16e3660fe3bdaa2890622e
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-07-25 16:31:10 -0600

    stream, onError,onDone and cancelOnError

--- 04_tipos_no_comunes/07_stream.dart
+++ 04_tipos_no_comunes/07_stream.dart
@@ -2,10 +2,20 @@ import 'dart:async';
 
 main() {
   var streamController = new StreamController();
-  streamController.stream.listen((data) {
-    print('Despegando!!! $data');
-  });
+  streamController.stream.listen((data) => print('Despegando!!! $data'),
+      onError: (err) {
+        print('super error');
+        print(err);
+      },
+      cancelOnError: false,
+      onDone: () => print('Mision Completa'));
 
   streamController.sink.add('Apollo 11');
+  streamController.sink.add('Apollo 12');
+  streamController.sink.add('Apollo 13');
+  streamController.sink.addError('Houston, we got a problem');
+  streamController.sink.add('Apollo 14');
+  streamController.sink.close();
+
   print('Fin del main');
 }
