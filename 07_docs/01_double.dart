f6a39fccabf9f4e87bd3fc829d61fcba8eea4197
Luis Rangel Castro <luisrangelc@rpaipos.com>
2020-08-01 14:09:04 -0600

    add double

---
+++ 07_docs/01_double.dart
@@ -0,0 +1,9 @@
+main() {
+  // double numero = 3.1416;
+  double numero = 10 / 3;
+  print('Firma: $numero');
+  print('Firma: ${numero.toString()}');
+  print('sign: ${numero.sign}');
+  print('isFinite: ${numero.isFinite}');
+  print('isInfinite: ${numero.isInfinite}');
+}
